f01573ac739530b56dea30a4e8c9d177d6906c02
Irvinsalvi <element025@hotmail.com>
2022-03-21 21:43:47 -0600

    Added files
    Added controller and User java files

--- src/module-info.java
+++ src/module-info.java
@@ -2,5 +2,6 @@ module ChatroomApplication {
 	requires javafx.controls;
 	requires javafx.fxml;
 	
+	opens application.controllers to javafx.fxml;
 	opens application to javafx.graphics, javafx.fxml;
 }
