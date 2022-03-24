13182e95221db2edeb653264bdf1724f2f9e1b93
Irvinsalvi <element025@hotmail.com>
2022-03-23 20:55:46 -0600

    adding icon to window

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module ChatroomApplication {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.base;
+	requires javafx.graphics;
 	
 	opens application.controllers to javafx.fxml;
 	opens application to javafx.graphics, javafx.fxml;
