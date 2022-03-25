cf2360e929c4d53f43bd0e6bb0d200df8770e03d
Irvinsalvi <element025@hotmail.com>
2022-03-24 18:16:33 -0600

    Updated Controller
    Added a Create account link to make it switch to a create account scene

--- src/application/controllers/SceneController.java
+++ src/application/controllers/SceneController.java
@@ -1,16 +1,37 @@
 package application.controllers;
 
+import java.io.IOException;
+
+import application.Main;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class SceneController
 {
-	public static void login(ActionEvent e)
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	public void login(ActionEvent e)
 	{
 		//this method will ran be once the users clicks on login
 		//several methods will be ran like checking if username is available
 		//making sure username field is not empty
 		//user gets connected to server
 		//switching to the next scene if the all the previous methods are met
-		//32131313123123131231231312321312
+	}
+	
+	public void createAccountScene(ActionEvent e) throws IOException
+	{
+		Parent root = FXMLLoader.load(Main.class.getResource("view/CreateAccount.fxml"));
+		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+		
 	}
 }
