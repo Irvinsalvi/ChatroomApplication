534ecfbf56365fda93e436ad5b7dceada344982e
Irvinsalvi <element025@hotmail.com>
2022-03-31 13:50:11 -0600

    UI updates
    Made some minor tweaks to the UI.

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,7 +6,6 @@ import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.layout.AnchorPane;
 
 
 public class Main extends Application {
