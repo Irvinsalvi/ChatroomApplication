7568c66cf1dc6da624fc7dc2778ecd58faf953fe
rashadramaileh <63267886+rashadramaileh@users.noreply.github.com>
2022-04-07 18:08:49 -0600

    updated create account and select picture screen

---
+++ src/application/controllers/CreateAccountController.java
@@ -0,0 +1,35 @@
+package application.controllers;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class CreateAccountController {
+	
+	SceneController switchScene = new SceneController();
+	
+	@FXML
+    private Button createAccountBtn;
+	
+	@FXML
+    private Button cancelAccountBtn;
+	
+	@FXML
+    private Button selectPictureBtn;
+	
+	@FXML
+	void onCreateAccountBtnClicked(ActionEvent event) throws IOException {
+			switchScene.chatRoomScene(event);
+	}
+	
+	@FXML
+	void onCancelAccountBtnClicked(ActionEvent event) throws IOException {
+			switchScene.loginFormScene(event);
+	}
+	
+	@FXML
+	void onSelectPictureBtnClicked(ActionEvent event) throws IOException {
+			switchScene.selectPictureScene(event);
+	}
+}
--- src/application/controllers/SceneController.java
+++ src/application/controllers/SceneController.java
@@ -53,6 +53,22 @@ public class SceneController
 	}
 	
 	/**
+	 * Shows chat room scene
+	 * 
+	 * @param e
+	 * @throws IOException
+	 */
+	public void selectPictureScene(ActionEvent e) throws IOException
+	{
+		root = FXMLLoader.load(Main.class.getResource("view/SelectPicture.fxml"));
+		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.setResizable(false);
+		stage.show();
+	}
+	
+	/**
 	 * Shows login form scene
 	 * 
 	 * @param event
@@ -67,5 +83,4 @@ public class SceneController
 		stage.setResizable(false);
 		stage.show();
 	}
-	
 }
